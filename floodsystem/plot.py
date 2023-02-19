ad02236c9af1406e3eee67add9f9f50296476be9
LionMalhamoub1 <115574734+LionMalhamoub1@users.noreply.github.com>
2023-02-19 12:19:23 +0000

    task 2e

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,7 +8,6 @@ def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels)
     plt.axhline(low, linestyle = "-", c ="red")
     plt.axhline(high, linestyle = "-", c = "yellow")
-    plt.legend("Water level data", "Typical Low", "Typical high")
     plt.xlabel("date")
     plt.ylabel("water level (m)")
     plt.xticks(rotation=45)
